a98b4c00e29aad325bf28892cefe949c48396b4c
yooonsu <jys_1109@naver.com>
2024-08-30 15:54:51 +0900

    memo

--- src/pages/ComponentTest.js
+++ src/pages/ComponentTest.js
@@ -1,23 +1,56 @@
 import React from 'react';
 import ListComponent from 'components/ListComponent';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import DraftsIcon from '@mui/icons-material/Drafts';
-import SendIcon from '@mui/icons-material/Send';
+import NoteAltIcon from '@mui/icons-material/NoteAlt';
 import { fetchRecords } from 'apis/RecordApi';
 
+// recordDate를 원하는 형식으로 변환하는 함수
+const formatDate = (dateString) => {
+  const date = new Date(dateString);
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  return `${year}.${month}.${day}`;
+};
+
+// 데이터를 월별로 그룹화하는 함수
+const groupByMonth = (data) => {
+  const groupedData = {};
+  data.forEach((item) => {
+    const date = new Date(item.recordDate);
+    const month = date.getMonth() + 1;
+    if (!groupedData[month]) {
+      groupedData[month] = [];
+    }
+    groupedData[month].push({
+      label: item.title,
+      date: formatDate(item.recordDate),
+      icon: <NoteAltIcon/>,
+    });
+  });
+
+    // 월별 데이터 정렬
+    Object.keys(groupedData).forEach((month) => {
+      groupedData[month].sort((a, b) => {
+        const dateA = Date.parse(a.date);
+        const dateB = Date.parse(b.date);
+        return dateA - dateB; // 오름차순 정렬
+      });
+    });
+
+    
+  return groupedData;
+};
+
 const ComponentTest = () => {
-    const [items, setItems] = React.useState([]);
+  const [groupedItems, setGroupedItems] = React.useState({});
 
     React.useEffect(() => {
         const getItems = async () => {
           try {
             const data = await fetchRecords();
-            const formattedData = data.map((item) => ({
-              label: item.label,
-              date: item.date,
-              icon: item.label === 'Sent mail' ? <SendIcon /> : item.label === 'Drafts' ? <DraftsIcon /> : <InboxIcon />, // 예시 아이콘 매칭
-            }));
-            setItems(formattedData);
+            const groupedData = groupByMonth(data);
+            setGroupedItems(groupedData);
+            console.log(groupedData);
           } catch (error) {
             console.error('Failed to fetch items:', error);
           }
@@ -28,8 +61,9 @@ const ComponentTest = () => {
 
       return (
         <div>
-          <h1>Dynamic MUI List</h1>
-          <ListComponent items={items} month={month} />
+          {Object.keys(groupedItems).map((month) => (
+            <ListComponent key={month} items={groupedItems[month]} month={month +'월'} />
+          ))}
         </div>
       );
 };
